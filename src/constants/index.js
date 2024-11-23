14c0b90d5dce626ef9d1eda0169923a62f52ec7b
Roman Pun <punroman11@gmail.com>
2024-11-23 11:47:33 +0545

    my first portfolio website

---
+++ src/constants/index.js
@@ -0,0 +1,263 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  threejs,
+  nextjs,
+  school,
+  mapty,
+  natours,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'works',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Web Developer',
+    icon: web,
+  },
+  {
+    title: 'React Developer',
+    icon: mobile,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'Lyrical Video Creator',
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'MongoDB',
+    icon: mongodb,
+  },
+  {
+    name: 'Three JS',
+    icon: threejs,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'Next Js',
+    icon: nextjs,
+  },
+];
+
+const experiences = [
+  {
+    title: 'React.js Developer',
+    company_name: 'Starbucks',
+    icon: starbucks,
+    iconBg: '#383E56',
+    date: 'March 2020 - April 2021',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'React Native Developer',
+    company_name: 'Tesla',
+    icon: tesla,
+    iconBg: '#E6DEDD',
+    date: 'Jan 2021 - Feb 2022',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'Web Developer',
+    company_name: 'Shopify',
+    icon: shopify,
+    iconBg: '#383E56',
+    date: 'Jan 2022 - Jan 2023',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'Full stack Developer',
+    company_name: 'Meta',
+    icon: meta,
+    iconBg: '#E6DEDD',
+    date: 'Jan 2023 - Present',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+    name: 'Sara Lee',
+    designation: 'CFO',
+    company: 'Acme Co',
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: 'Chris Brown',
+    designation: 'COO',
+    company: 'DEF Corp',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: 'Lisa Wang',
+    designation: 'CTO',
+    company: '456 Enterprises',
+    image: 'https://randomuser.me/api/portraits/women/6.jpg',
+  },
+];
+
+const projects = [
+  {
+    name: 'Tours App',
+    description:
+      'A comprehensive travel booking platform that allows users to book tours, providing a convenient and efficient solution for travel needs.',
+    tags: [
+      {
+        name: 'javascript',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'parcel',
+        color: 'pink-text-gradient',
+      },
+      {
+        name: 'pug',
+        color: 'gray-text-gradient ',
+      },
+      {
+        name: 'nodejs',
+        color: 'orange-text-gradient ',
+      },
+    ],
+    image: natours,
+    source_code_link: 'https://github.com/r0manpun/tours-app',
+  },
+  {
+    name: 'Mapty || Map your workout',
+    description:
+      'A web application that allows users to log their hiking and biking routes on a map.',
+    tags: [
+      {
+        name: 'javascript',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'leafletjs',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'html',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: mapty,
+    source_code_link: 'https://github.com/r0manpun/mapty',
+  },
+  {
+    name: 'School Website',
+    description:
+      'A detailed information platform that allows users to know school moto, education quality, facilities and history.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'html',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwindcss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: school,
+    source_code_link: 'https://github.com/r0manpun/school-website',
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
